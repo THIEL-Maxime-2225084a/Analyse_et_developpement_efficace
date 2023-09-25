773bbb625fe43188bc7cb34e2c7c5d475e557b91
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2023-09-25 11:51:02 +0200

    Update ListeSimpleString.java

--- TD Structure de donnees/src/listeSimple/ListeSimpleString.java
+++ TD Structure de donnees/src/listeSimple/ListeSimpleString.java
@@ -10,4 +10,42 @@ public class ListeSimpleString {
 	public ListeSimpleString(NodeString head) {
 		this.head = new NodeString(element, null);
 	}
+	
+	public void insererElementEnTete(String elem) {
+		NodeString newNode = new NodeString(elem, head);
+		head = newNode;
+		size++;
+	}
+	
+	public String getElementTete() {
+		return head.getElement();
+	}
+	
+	public String getAvantDernierElement() {
+		NodeString courant = head;
+		//si le noeud de tête n'a pas de suivant alors la liste ne contient
+		//qu'un seule élément. Il n'y a pas d'avant-dernier possible puisque le
+		//premier est le même que le dernier
+		if (courant.getNext() == null) {
+			return null;
+		}
+		while(courant.getNext()!=null) {
+			prec = courant;
+			courant = courant.getNext();
+		}
+		return prec.getElement();
+	}
+	
+	public void inverser() {
+		NodeString avant = null, courant = this.head, suivant;
+		while (courant != null) {
+			suivant = courant.getNext();
+			courant.setNext(avant);
+			avant = courant;
+			courant = suivant;
+		}
+		this.head = avant;
+	}
+	
+	
 }
