5a95e9836c0dc3493919ed511aeffc67f38bc54f
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2023-09-25 11:51:24 +0200

    Update ListeSimpleStringTest.java

--- TD Structure de donnees/src/listeSimple/ListeSimpleStringTest.java
+++ TD Structure de donnees/src/listeSimple/ListeSimpleStringTest.java
@@ -1,38 +1,14 @@
 package listeSimple;
 
-public class ListeSimpleString {
-	private NodeString head;
-	
-	public ListeSimpleString() {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListeSimpleStringTest {
+
+	@Test
+	void testListeSimpleStringConstructeur() {
+		ListeSimpleString l = new ListeSimpleString("13");
+		assertNotEquals(null, l);
+	}
 		
-	}
-	
-	public ListeSimpleString(NodeString head) {
-		this.head = new NodeString(element, null);
-	}
-	
-	public void insererElementEnTete(String elem) {
-		NodeString newNode = new NodeString(elem, head);
-		head = newNode;
-		size++;
-	}
-	
-	public String getElementTete() {
-		return head.getElement();
-	}
-	
-	public String getAvantDernierElement() {
-		NodeString courant = head;
-		//si le noeud de tête n'a pas de suivant alors la liste ne contient
-		//qu'un seule élément. Il n'y a pas d'avant-dernier possible puisque le
-		//premier est le même que le dernier
-		if (courant.getNext() == null) {
-			return null;
-		}
-		while(courant.getNext()!=null) {
-			courant = courant.getNext();
-		}
-	}
-	
-	
-}
